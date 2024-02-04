eb99262a91215853cfe8c711c0d2a4dbc99a2f71
Domok23 <wahyur2bro@gmail.com>
2024-02-04 22:06:50 +0700

    redesign UI beranda 1.2.2

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -69,9 +69,9 @@
         @if($key % 2 == 0)
         <div class="row featurette">
             <div class="col-md-7">
-                <h2 class="featurette-heading">{{ $item->judul }}</h2>
+                <h2 class="featurette-heading mt-2">{{ $item->judul }}</h2>
                 <p class="lead">{{ $item->kutipan }}</p>
-                <a class="btn btn-success" href="/artikel/{{ $item->id }}">Lebih lanjut →</a>
+                <a class="btn btn-success mb-4" href="/artikel/{{ $item->id }}">Lebih lanjut →</a>
             </div>
             <div class="col-md-5">
                 <div class="thumbnail">
@@ -86,9 +86,9 @@
         @else
         <div class="row featurette">
             <div class="col-md-7 order-md-2">
-                <h2 class="featurette-heading">{{ $item->judul }}</h2>
+                <h2 class="featurette-heading mt-2">{{ $item->judul }}</h2>
                 <p class="lead">{{ $item->kutipan }}</p>
-                <a class="btn btn-success" href="/artikel/{{ $item->id }}">Lebih lanjut →</a>
+                <a class="btn btn-success mb-4" href="/artikel/{{ $item->id }}">Lebih lanjut →</a>
             </div>
             <div class="col-md-5">
                 <div class="thumbnail">
